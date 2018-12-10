40b4c4deb5fe4a2605f5d65f450344c367988321
yixiang <yixiang@didichuxing.com>
2018-12-10 21:34:27 +0800

    检测历史 ： 正在开发中 敬请期待

--- DoraemonKit/Src/Core/Plugin/FPS/List/DoraemonFPSListViewController.m
+++ DoraemonKit/Src/Core/Plugin/FPS/List/DoraemonFPSListViewController.m
@@ -6,6 +6,7 @@
 //
 
 #import "DoraemonFPSListViewController.h"
+#import "DoraemonDefine.h"
 
 @interface DoraemonFPSListViewController ()
 
@@ -16,6 +17,13 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.title = @"检测记录";
+    
+    UILabel *titleLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, IPHONE_NAVIGATIONBAR_HEIGHT+kDoraemonSizeFrom750(40), self.view.doraemon_width, kDoraemonSizeFrom750(40))];
+    titleLabel.font = [UIFont systemFontOfSize:kDoraemonSizeFrom750(32)];
+    titleLabel.textColor = [UIColor doraemon_black_1];
+    titleLabel.text = @"正在开发中,敬请期待";
+    titleLabel.textAlignment = NSTextAlignmentCenter;
+    [self.view addSubview:titleLabel];
 }
 
 
