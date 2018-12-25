35f044cc54ae2f7943001e9879398f89fc15d43f
yixiang <yixiang@didichuxing.com>
2018-12-25 10:40:48 +0800

    添加注释

--- iOS/DoraemonKit/Src/Core/Base/DoraemonBaseViewController.m
+++ iOS/DoraemonKit/Src/Core/Base/DoraemonBaseViewController.m
@@ -47,6 +47,7 @@
 
 - (void)viewWillDisappear:(BOOL)animated{
     [super viewWillDisappear:animated];
+    //输入框聚焦的时候，会把当前window设置为keyWindow，我们在当页面消失的时候，判断一下，把keyWindow交还给[[UIApplication sharedApplication].delegate window]
     UIWindow *appWindow = [[UIApplication sharedApplication].delegate window];
     UIWindow *keyWindow = [UIApplication sharedApplication].keyWindow;
     if (appWindow != keyWindow) {
