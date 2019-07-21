809d368dd259b840473b0a40f817adcfa61ba09a
yixiang <yixiang@didichuxing.com>
2019-07-21 22:41:43 +0800

    更新版本号

--- iOS/DoraemonKit/Src/Core/Util/DoraemonStatisticsUtil.m
+++ iOS/DoraemonKit/Src/Core/Util/DoraemonStatisticsUtil.m
@@ -26,7 +26,7 @@
     
     NSString *appId = [[NSBundle mainBundle] bundleIdentifier];;
     NSString *appName = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleDisplayName"];
-    NSString *doKitVersion = @"1.2.0";
+    NSString *doKitVersion = @"1.2.1";
     NSString *type = @"iOS";
     NSString *from = @"1";
     
