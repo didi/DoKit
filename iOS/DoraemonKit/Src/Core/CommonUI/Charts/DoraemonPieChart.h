525bc9cb2d662d895342257eb462e8e4d59ba537
0xd-cc <i@0xd.cc>
2019-09-28 19:39:53 +0800

    feat: 移除PNChart依赖

---
+++ iOS/DoraemonKit/Src/Core/CommonUI/Charts/DoraemonPieChart.h
@@ -0,0 +1,21 @@
+//
+//  PieChart.h
+//  DoraemonKit
+//
+//  Created by 0xd on 2019/9/25.
+//  Copyright © 2019 000. All rights reserved.
+//
+
+#import "DoraemonChart.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DoraemonPieChart : DoraemonChart
+@property (nonatomic, strong) UIFont *itemDescriptionFont;
+@property (nonatomic, strong) UIColor *itemDescriptionTextColor;
+
+@property (nonatomic, assign) CGFloat innerCircleRadiusRatio;
+
+@end
+
+NS_ASSUME_NONNULL_END
