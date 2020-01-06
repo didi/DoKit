05223b96008b82bf790759d2b1c7e1e8740c3154
ydlsl <1341521959@qq.com>
2020-01-06 20:46:23 +0800

    test for UIMenuController

--- iOS/DoraemonKitDemo/DoraemonKitDemo/DemoVC/UI/DoraemonDemoUIViewController.m
+++ iOS/DoraemonKitDemo/DoraemonKitDemo/DemoVC/UI/DoraemonDemoUIViewController.m
@@ -35,7 +35,59 @@
     titleLabel.textColor = [UIColor doraemon_colorWithString:@"#FF0000"];
     [self.view addSubview:titleLabel];
     
+    UITextField *input = [[UITextField alloc] initWithFrame:CGRectMake(100, 300, 200, 50)];
+    input.textAlignment = NSTextAlignmentCenter;
+    input.keyboardType = UIKeyboardTypeNumberPad;
+    input.backgroundColor  = [UIColor lightGrayColor];
+    [self.view addSubview:input];
+    
+    UIButton *button = [[UIButton alloc] initWithFrame:CGRectMake(200, 200, 200, 50)];
+    button.backgroundColor = [UIColor lightGrayColor];
+    button.layer.cornerRadius = 8;
+    [button setTitle:@"UIMenuController测试" forState:UIControlStateNormal];
+    [button addTarget:self action:@selector(deleteBtnAction:) forControlEvents:UIControlEventTouchUpInside];
+    
+    [self.view addSubview:button];
+    
+    
 }
+
+
+- (void)deleteBtnAction:(UIButton *)deleteBtn
+{
+    [self becomeFirstResponder];// 用于UIMenuController显示，缺一不可
+    UIMenuController *menu = [UIMenuController sharedMenuController];
+    UIMenuItem *item1 = [[UIMenuItem alloc] initWithTitle:@"撤销" action:@selector(revokeAction)];
+    UIMenuItem *item2 = [[UIMenuItem alloc] initWithTitle:@"确认" action:@selector(sureAction)];
+    menu.menuItems = @[item1, item2];
+    menu.arrowDirection = UIMenuControllerArrowUp;
+    [menu setTargetRect:deleteBtn.frame inView:deleteBtn.superview];//  [menu setTargetRect:所点击的按钮Frame inView:按钮的父视图];
+    [menu setMenuVisible:YES animated:YES];
+}
+
+- (BOOL)canBecomeFirstResponder{
+    return YES;
+ }
+
+- (BOOL)canPerformAction:(SEL)action withSender:(id)sender
+{
+    if (action == @selector(revokeAction)) {
+        return YES;
+    }
+    if (action == @selector(sureAction)) {
+        return YES;
+    }
+    return NO;//隐藏系统默认的菜单项
+}
+
+- (void)revokeAction{
+    NSLog(@"选择了撤销");
+}
+
+- (void)sureAction{
+    NSLog(@"选择了删除");
+}
+
 
 
 @end
