ee24a77cefb161bb6dbfbfb58bcdfc81b7881776
yixiangboy <919727170@qq.com>
2020-02-07 16:55:06 +0800

    如果没有填写pId的话，弹出toast  需要到www.dokit.cn上注册pId才能使用该功能

--- iOS/DoraemonKit/Src/Core/Plugin/Health/DoraemonHealthPlugin.m
+++ iOS/DoraemonKit/Src/Core/Plugin/Health/DoraemonHealthPlugin.m
@@ -8,12 +8,20 @@
 #import "DoraemonHealthPlugin.h"
 #import "DoraemonHomeWindow.h"
 #import "DoraemonHealthViewController.h"
+#import "DoraemonManager.h"
+#import "DoraemonToastUtil.h"
+#import "UIViewController+Doraemon.h"
 
 @implementation DoraemonHealthPlugin
 
 - (void)pluginDidLoad{
-    DoraemonHealthViewController *vc = [[DoraemonHealthViewController alloc] init];
-    [DoraemonHomeWindow openPlugin:vc];
+    if ([DoraemonManager shareInstance].pId) {
+        DoraemonHealthViewController *vc = [[DoraemonHealthViewController alloc] init];
+        [DoraemonHomeWindow openPlugin:vc];
+    }else{
+        [DoraemonToastUtil showToastBlack:@"需要到www.dokit.cn上注册pId才能使用该功能" inView:[UIViewController rootViewControllerForDoraemonHomeWindow].view];
+    }
+
 }
 
 @end
--- iOS/DoraemonKit/Src/Core/Plugin/Mock/DoraemonMockPlugin.m
+++ iOS/DoraemonKit/Src/Core/Plugin/Mock/DoraemonMockPlugin.m
@@ -8,12 +8,20 @@
 #import "DoraemonMockPlugin.h"
 #import "DoraemonMockViewController.h"
 #import "DoraemonHomeWindow.h"
+#import "DoraemonManager.h"
+#import "DoraemonToastUtil.h"
+#import "UIViewController+Doraemon.h"
 
 @implementation DoraemonMockPlugin
 
 - (void)pluginDidLoad{
-    DoraemonMockViewController *vc = [[DoraemonMockViewController alloc] init];
-    [DoraemonHomeWindow openPlugin:vc];
+    if ([DoraemonManager shareInstance].pId) {
+        DoraemonMockViewController *vc = [[DoraemonMockViewController alloc] init];
+        [DoraemonHomeWindow openPlugin:vc];
+    }else{
+        [DoraemonToastUtil showToastBlack:@"需要到www.dokit.cn上注册pId才能使用该功能" inView:[UIViewController rootViewControllerForDoraemonHomeWindow].view];
+    }
+
 }
 
 @end
