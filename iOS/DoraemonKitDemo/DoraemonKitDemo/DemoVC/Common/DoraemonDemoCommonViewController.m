b6a5fe0eb53539988a51c7ce3a0ea799a0684818
yixiangboy <919727170@qq.com>
2020-03-19 11:15:06 +0800

    完善测试代码

--- iOS/DoraemonKitDemo/DoraemonKitDemo/DemoVC/Common/DoraemonDemoCommonViewController.m
+++ iOS/DoraemonKitDemo/DoraemonKitDemo/DemoVC/Common/DoraemonDemoCommonViewController.m
@@ -37,6 +37,10 @@
     [btn2 setTitle:DoraemonLocalizedString(@"隐藏入口") forState:UIControlStateNormal];
     [btn2 addTarget:self action:@selector(hiddenEntry) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:btn2];
+    
+    UIImageView *imgView = [[UIImageView alloc] initWithFrame:CGRectMake(0, btn2.doraemon_bottom+60, 100, 100)];
+    imgView.image = [[self class] jy_QRCodeFromString:@"hello" size:100];
+    [self.view addSubview:imgView];
 
 }
 
@@ -54,6 +58,59 @@
 - (void)hiddenEntry{
     [[DoraemonManager shareInstance] hiddenDoraemon];
 }
+
+/**
+*  根据字符串生成二维码图片
+*
+*  @param code 二维码code
+*  @param size 生成图片大小
+*
+*  @return image
+*/
++ (UIImage *)jy_QRCodeFromString:(NSString *)code size:(CGFloat)size{
+    //创建CIFilter 指定filter的名称为CIQRCodeGenerator
+    CIFilter *filter = [CIFilter filterWithName:@"CIQRCodeGenerator"];
+    //指定二维码的inputMessage,即你要生成二维码的字符串
+    [filter setValue:[code dataUsingEncoding:NSUTF8StringEncoding] forKey:@"inputMessage"];
+    //输出CIImage
+    CIImage *ciImage = [filter outputImage];
+    //对CIImage进行处理
+    return [self createfNonInterpolatedImageFromCIImage:ciImage withSize:size];
+}
+
+/**
+ *  对CIQRCodeGenerator 生成的CIImage对象进行不插值放大或缩小处理
+ *
+ *  @param iamge 原CIImage对象
+ *  @param size  处理后的图片大小
+ *
+ *  @return image
+ */
++ (UIImage *) createfNonInterpolatedImageFromCIImage:(CIImage *)iamge withSize:(CGFloat)size{
+    CGRect extent = iamge.extent;
+    CGFloat scale = MIN(size/CGRectGetWidth(extent), size/CGRectGetHeight(extent));
+    size_t with = scale * CGRectGetWidth(extent);
+    size_t height = scale * CGRectGetHeight(extent);
+    
+    UIGraphicsBeginImageContext(CGSizeMake(with, height));
+    CGContextRef bitmapContextRef = UIGraphicsGetCurrentContext();
+    
+    CIContext *context = [CIContext contextWithOptions:nil];
+    //通过CIContext 将CIImage生成CGImageRef
+    CGImageRef bitmapImage = [context createCGImage:iamge fromRect:extent];
+    //在对二维码放大或缩小处理时,禁止插值
+    CGContextSetInterpolationQuality(bitmapContextRef, kCGInterpolationNone);
+    //对二维码进行缩放
+    CGContextScaleCTM(bitmapContextRef, scale, scale);
+    //将二维码绘制到图片上下文
+    CGContextDrawImage(bitmapContextRef, extent, bitmapImage);
+    //获得上下文中二维码
+    UIImage *retVal =  UIGraphicsGetImageFromCurrentImageContext();
+    CGImageRelease(bitmapImage);
+    CGContextRelease(bitmapContextRef);
+    return retVal;
+}
+
 
 
 @end
