8d2c4775142709195d3c020731e9edb4c6de8feb
yixiangboy <919727170@qq.com>
2020-04-13 11:22:24 +0800

    fix demo

--- iOS/DoraemonKitDemo/DoraemonKitDemo/DoKitAppDelegate.m
+++ iOS/DoraemonKitDemo/DoraemonKitDemo/DoKitAppDelegate.m
@@ -37,7 +37,7 @@
     }
     [[DoraemonManager shareInstance] addPluginWithTitle:DoraemonDemoLocalizedString(@"测试插件") icon:@"doraemon_default" desc:DoraemonDemoLocalizedString(@"测试插件") pluginName:@"TestPlugin" atModule:DoraemonDemoLocalizedString(@"业务工具")];
 
-    [[DoraemonManager shareInstance] addPluginWithTitle:DoraemonDemoLocalizedString(@"block方式加入插件") icon:@"doraemon_default" desc:@"测试插件" pluginName:@"TestPlugin" atModule:DoraemonDemoLocalizedString(@"业务工具") handle:^(NSDictionary *itemData) {
+    [[DoraemonManager shareInstance] addPluginWithTitle:DoraemonDemoLocalizedString(@"block方式加入插件") icon:@"doraemon_default" desc:@"测试插件" pluginName:@"pluginName" atModule:DoraemonDemoLocalizedString(@"业务工具") handle:^(NSDictionary *itemData) {
         NSLog(@"handle block plugin");
     }];
 
