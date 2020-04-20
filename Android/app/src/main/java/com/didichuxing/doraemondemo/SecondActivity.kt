be715d3c15cb60aced8d638989b41b5d5df95c71
jackjintai <jackjintai@didiglobal.com>
2020-04-20 19:40:07 +0800

    android: fixed main icon not show

--- Android/app/src/main/java/com/didichuxing/doraemondemo/SecondActivity.kt
+++ Android/app/src/main/java/com/didichuxing/doraemondemo/SecondActivity.kt
@@ -1,12 +1,9 @@
 package com.didichuxing.doraemondemo
 
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import com.blankj.utilcode.util.ToastUtils
 import com.didichuxing.doraemonkit.DoraemonKit
 import kotlinx.android.synthetic.main.activity_second.*
-import me.imid.swipebacklayout.lib.app.SwipeBackActivity
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
