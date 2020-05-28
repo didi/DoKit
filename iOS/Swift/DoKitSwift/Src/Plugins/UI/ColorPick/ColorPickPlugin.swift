23262182d0e4c7c57789f73f2642d5ecdd6ea1e5
方林威 <calm1993@163.com>
2020-05-28 17:42:28 +0800

    优化语法

--- iOS/Swift/DoKitSwift/Src/Plugins/UI/ColorPick/ColorPickPlugin.swift
+++ iOS/Swift/DoKitSwift/Src/Plugins/UI/ColorPick/ColorPickPlugin.swift
@@ -9,17 +9,11 @@ import Foundation
 
 struct ColorPickPlugin: Plugin {
 
-    var module: String {
-        return DoKitLocalizedString("视觉工具")
-    }
+    var module: String { DoKitLocalizedString("视觉工具") }
     
-    var title: String {
-        return DoKitLocalizedString("取色器")
-    }
+    var title: String { DoKitLocalizedString("取色器") }
     
-    var icon: UIImage? {
-        return UIImage.dokitImageNamed(name: "doraemon_setting")
-    }
+    var icon: UIImage? { UIImage.dokitImageNamed(name: "doraemon_setting") }
     
     func didLoad() {
         ColorPickWindow.shared.show()
