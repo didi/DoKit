bbfdcbe1474a4180994a83a3c39535966e19fd4a
LY <coderyliu@gmail.com>
2020-05-30 01:42:44 +0800

    feat: 完成H5任意门搜索历史记录功能

--- iOS/Swift/DoKitSwift/Src/Plugins/Common/H5/H5ViewController.swift
+++ iOS/Swift/DoKitSwift/Src/Plugins/Common/H5/H5ViewController.swift
@@ -15,6 +15,9 @@ class H5ViewController: BaseViewController {
     var lineView: UIView!
     var jumpBtn: UIButton!
     var scanJumpBtn: UIButton!
+    var historyTableView: UITableView!
+    
+    var historyArray: [String]?
     
     // MARK: - Life Cycle Methods
     override func viewDidLoad() {
@@ -23,6 +26,12 @@ class H5ViewController: BaseViewController {
         self.setTitle(title: LocalizedString("H5任意门"))
         
         initUI()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.historyArray = CacheManager.shared.h5historicalRecord
+        self.historyTableView.reloadData()
     }
     
     // MARK: - Override Methods
@@ -60,10 +69,16 @@ class H5ViewController: BaseViewController {
         }
         
         let h5Url = h5UrlTextView.text
-        
+        CacheManager.shared.saveH5historicalRecord(text: h5Url)
         let vc = DefaultWebViewController()
         vc.url = urlCorrectionWithURL(URL: h5Url ?? "")
         self.navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    @objc func clearRecordBtnClickHandle(sender: UIButton) -> Void {
+        CacheManager.shared.clearAllH5historicalRecord()
+        self.historyArray = CacheManager.shared.h5historicalRecord
+        self.historyTableView.reloadData()
     }
     
     // MARK: - Private Methods
@@ -121,5 +136,64 @@ extension H5ViewController {
         scanJumpBtn.setBackgroundImage(UIImage("doraemon_scan"), for: .normal)
         scanJumpBtn.addTarget(self, action: #selector(clickScanBtnClickHandle), for: .touchUpInside)
         self.view.addSubview(scanJumpBtn)
+        
+        // HistoryTableView
+        historyTableView = UITableView(frame: CGRect(x: 0, y: lineView.bottom + kSizeFrom750_Landscape(32.0), width: view.width, height: jumpBtn.top - lineView.bottom - kSizeFrom750_Landscape(32.0)))
+        historyTableView.dataSource = self
+        historyTableView.delegate = self
+        historyTableView.register(UITableViewCell.self, forCellReuseIdentifier: Identifier)
+        self.view.addSubview(historyTableView)
+        
+        self.view.bringSubviewToFront(jumpBtn)
+    }
+}
+
+// MARK: - UITableViewDataSource, UITableViewDelegate
+extension H5ViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return historyArray?.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCell(withIdentifier: Identifier)
+        if cell == nil {
+            cell = UITableViewCell(style: .default, reuseIdentifier: Identifier)
+        }
+        cell?.textLabel?.text =  historyArray?[indexPath.row]
+        cell?.textLabel?.textColor = UIColor.init(0x333333, alphaValue: 1.0)
+        cell?.textLabel?.font = UIFont.systemFont(ofSize: kSizeFrom750_Landscape(30.0))
+        cell?.imageView?.image = UIImage("doraemon_search")
+        return cell!
+    }
+    
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return kSizeFrom750_Landscape(73.0)
+    }
+    
+    func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        let footerView = UIView(frame: CGRect(x: 0, y: 0, width: view.width, height: kSizeFrom750_Landscape(73.0)))
+        
+        let clearButton = UIButton(frame: CGRect(x: (view.width - kSizeFrom750_Landscape(300.0)) / 2, y: kSizeFrom750_Landscape(40.0), width: kSizeFrom750_Landscape(300.0), height: 33.0))
+        clearButton.setTitle("清除搜索历史", for: .normal)
+        clearButton.setTitleColor(UIColor.init(0x999999), for: .normal)
+        clearButton.titleLabel?.font = UIFont.systemFont(ofSize: kSizeFrom750_Landscape(24.0))
+        clearButton.addTarget(self, action: #selector(clearRecordBtnClickHandle(sender:)), for: .touchUpInside)
+        footerView.addSubview(clearButton)
+        
+        return footerView
+    }
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, titleForDeleteConfirmationButtonForRowAt indexPath: IndexPath) -> String? {
+        return LocalizedString("删除")
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        CacheManager.shared.clearH5historicalRecord(text: self.historyArray![indexPath.row])
+        self.historyArray = CacheManager.shared.h5historicalRecord
+        self.historyTableView.reloadData()
     }
 }
