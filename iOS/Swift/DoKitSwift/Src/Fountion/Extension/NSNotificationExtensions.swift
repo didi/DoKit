4f6eb5c687522c50df74e049c4d1d1dbf910ea1c
方林威 <calm1993@163.com>
2020-05-29 12:19:56 +0800

    完成ColorPickInfoView布局

---
+++ iOS/Swift/DoKitSwift/Src/Fountion/Extension/NSNotificationExtensions.swift
@@ -0,0 +1,16 @@
+//
+//  NSNotificationExtensions.swift
+//  DoraemonKit-Swift
+//
+//  Created by 方林威 on 2020/5/29.
+//
+
+import Foundation
+
+extension NSNotification.Name {
+    
+    static let closePluginNotification = NSNotification.Name("DoraemonClosePluginNotification")
+    static let quickOpenLogVCNotification = NSNotification.Name("DoraemonQuickOpenLogVCNotification")
+    static let managerUpdateNotification = NSNotification.Name("DoraemonKitManagerUpdateNotification")
+}
+
