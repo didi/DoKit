4321edf35bbc4b51823e91f50165e4175302ea62
方林威 <calm1993@163.com>
2020-05-30 20:33:57 +0800

    完成crash捕获函数

--- iOS/Swift/DoKitSwift/Src/Plugins/Performance/Crash/Function/CrashUncaughtExceptionHandler.swift
+++ iOS/Swift/DoKitSwift/Src/Plugins/Performance/Crash/Function/CrashUncaughtExceptionHandler.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+
+enum CrashUncaughtExceptionHandler {
+    static var previousUncaughtExceptionHandler = NSGetUncaughtExceptionHandler()
+    
+    static func registerHandler() {
+        NSSetUncaughtExceptionHandler(uncaughtExceptionHandler)
+    }
+}
+
+func uncaughtExceptionHandler(exception: NSException) {
+    
+    let info =
+    """
+        ========uncaughtException异常错误报告========
+        name: \(exception.name)
+        reason: \(exception.reason ?? "unknown")
+        callStackSymbols:
+        \(exception.callStackSymbols.joined(separator: "\n"))
+    """
+    // 保存崩溃日志到沙盒cache目录
+    print(exception)
+    CrashTool.save(crash: info, file: "Crash(Uncaught)")
+    // 调用之前崩溃的回调函数
+    CrashUncaughtExceptionHandler.previousUncaughtExceptionHandler?(exception)
+    
+    // 杀掉程序，这样可以防止同时抛出的SIGABRT被SignalException捕获
+    kill(getpid(), SIGKILL);
+ }
