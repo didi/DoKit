c97f364683408662022111f1883d9476c3bd038f
方林威 <calm1993@163.com>
2020-05-30 18:35:22 +0800

    构建crash页面

--- iOS/Swift/DoKitSwift/Src/Fountion/Extension/UIViewControllerExtensions.swift
+++ iOS/Swift/DoKitSwift/Src/Fountion/Extension/UIViewControllerExtensions.swift
@@ -44,4 +44,27 @@ extension UIViewController {
             return nil
         }
     }
+    
+    @discardableResult
+    func showAlert(title: String?, message: String?, buttonTitles: [String]? = nil, highlightedButtonIndex: Int? = nil, completion: ((Int) -> Void)? = nil) -> UIAlertController {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        var allButtons = buttonTitles ?? [String]()
+        if allButtons.count == 0 {
+            allButtons.append("OK")
+        }
+
+        for index in 0..<allButtons.count {
+            let buttonTitle = allButtons[index]
+            let action = UIAlertAction(title: buttonTitle, style: .default, handler: { (_) in
+                completion?(index)
+            })
+            alertController.addAction(action)
+            // Check which button to highlight
+            if let highlightedButtonIndex = highlightedButtonIndex, index == highlightedButtonIndex {
+                alertController.preferredAction = action
+            }
+        }
+        present(alertController, animated: true, completion: nil)
+        return alertController
+    }
 }
--- iOS/Swift/DoKitSwift/Src/Plugins/Performance/Crash/CrashViewController.swift
+++ iOS/Swift/DoKitSwift/Src/Plugins/Performance/Crash/CrashViewController.swift
@@ -9,21 +9,129 @@ import UIKit
 
 class CrashViewController: BaseViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        title = "Crash"
+    private lazy var tableView: UITableView = {
+        $0.rowHeight = 52
+        $0.dataSource = self
+        $0.delegate = self
+        $0.register(CrashListCell.self, forCellReuseIdentifier: CrashListCell.identifier)
+        return $0
+    }(UITableView(frame: .zero, style: .plain) )
+    
+    private static var isCache: Bool {
+        get { UserDefaults.standard.bool(forKey: "Dokit.CrashCache.isOn") }
+        set { UserDefaults.standard.set(newValue, forKey: "Dokit.CrashCache.isOn")}
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private var rows: [Row] = [.switch(isOn: isCache), .log, .clean]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setup()
     }
-    */
+    
+    private func setup() {
+        if #available(iOS 11.0, *) {
+            navigationController?.navigationBar.prefersLargeTitles = true
+        }
+        
+        setTitle(title: LocalizedString("Crash"))
+        view.addSubview(tableView)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
+    }
+}
 
+// MARK:- UITableViewDataSource & UITableViewDelegate
+extension CrashViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return rows.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: CrashListCell.identifier,
+                                                 for: indexPath) as! CrashListCell
+        
+        let row = rows[indexPath.row]
+        switch row {
+        case .switch(let isOn):
+            cell.delegate = self
+            cell.set(.switch(isOn: isOn))
+            
+        default:                    cell.set(.indicator)
+        }
+        cell.set(text: row.title)
+        return cell
+    }
+}
+
+extension CrashViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        switch rows[indexPath.row] {
+        case .log:
+            navigationController?.pushViewController(CrashListViewController(), animated: true)
+            
+        case .clean:
+            showAlert(
+                title: LocalizedString("提示"),
+                message: LocalizedString("确认删除所有崩溃日志吗？"),
+                buttonTitles: ["取消", "确定"]
+            ) { index in
+                switch index {
+                case 1:
+                    print("删除")
+                    
+                default:
+                    break
+                }
+            }
+            
+        case .switch:
+            break
+        }
+    }
+}
+
+extension CrashViewController: CrashListCellDelegate {
+    
+    func switchAction(cell: CrashListCell, _ isOn: Bool, handle: @escaping (Bool) -> Void) {
+        showAlert(
+            title: LocalizedString("提示"),
+            message: LocalizedString("该功能需要重启App才能生效"),
+            buttonTitles: ["取消", "确定"]
+        ) { index in
+            switch index {
+            case 1:
+                CrashViewController.isCache = true
+                handle(true)
+                exit(0)
+                
+            default:
+                CrashViewController.isCache = false
+                handle(false)
+            }
+        }
+    }
+}
+
+extension CrashViewController {
+    
+    enum Row {
+        case `switch`(isOn: Bool)
+        case log
+        case clean
+        
+        var title: String {
+            switch self {
+            case .switch:       return LocalizedString("Crash日志收集开关")
+            case .log:          return LocalizedString("查看Crash日志")
+            case .clean:        return LocalizedString("一键清理Crash日志")
+            }
+        }
+    }
 }
