630633f36ff7c2580933b78bba38c90313629398
Ailsa <15623247725@163.com>
2020-06-15 14:47:30 +0800

    Add: 健康体检/构建文件目录

---
+++ iOS/Swift/DoKitSwift/Src/Plugins/Platform/Health/View/Home/Detail/HealthStartingTitle.swift
@@ -0,0 +1,20 @@
+//
+//  HealthStartingTitle.swift
+//  DoraemonKit-Swift
+//
+//  Created by Ailsa on 2020/6/15.
+//
+
+import UIKit
+
+class HealthStartingTitle: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ iOS/Swift/DoKitSwift/Src/Plugins/Platform/Health/View/Home/HealthHomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HealthHomeView.swift
+//  DoraemonKit-Swift
+//
+//  Created by Ailsa on 2020/6/15.
+//
+
+import UIKit
+
+class HealthHomeView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
