f50d4a0d7e8b816d6977d5083d608c68e7f0add5
guoyuanyuan <yuanyuanxingkong@163.com>
2020-06-22 17:27:04 +0800

    代码风格修改

--- iOS/Swift/DoKitSwift/Src/Plugins/Common/Logs/Views/LogListCell.swift
+++ iOS/Swift/DoKitSwift/Src/Plugins/Common/Logs/Views/LogListCell.swift
@@ -8,8 +8,22 @@
 import UIKit
 
 class LogListCell: UITableViewCell {
-    var arrowImage:UIImageView?
-    var logLabel:UILabel?
+    static let identifier: String = "logListCellID"
+    private lazy var arrowImage: UIImageView = {
+        $0.image = DKImage(named: "doraemon_expand_no")
+        $0.contentMode = .center
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        return $0
+    }(UIImageView())
+    
+    private lazy var logLabel: UILabel = {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.textColor = .black
+        $0.font = .systemFont(ofSize: 12)
+        $0.numberOfLines = 0;
+        return $0
+    }(UILabel())
+
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         initUI()
@@ -18,38 +32,30 @@ class LogListCell: UITableViewCell {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
    
     private func initUI() {
-        self.translatesAutoresizingMaskIntoConstraints = false
-        arrowImage = UIImageView.init()
-        arrowImage!.image = DKImage(named: "doraemon_expand_no")
-        arrowImage?.contentMode = .center
-        arrowImage?.translatesAutoresizingMaskIntoConstraints = false
-        self.contentView.addSubview(arrowImage!)
-        self.contentView.addConstraint(NSLayoutConstraint.init(item: arrowImage as Any, attribute: .leading, relatedBy: .equal, toItem: self.contentView, attribute: .leading, multiplier: 1.0, constant: 15))
-        self.contentView.addConstraint(NSLayoutConstraint.init(item: arrowImage!, attribute: .top, relatedBy: .equal, toItem: self.contentView, attribute: .top, multiplier: 1.0, constant: 8))
-        self.contentView.addConstraint(NSLayoutConstraint.init(item: arrowImage!, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: 25))
-        self.contentView.addConstraint(NSLayoutConstraint.init(item: arrowImage!, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: 25))
+        translatesAutoresizingMaskIntoConstraints = false
+        contentView.addSubview(arrowImage)
+        contentView.addSubview(logLabel)
+    
+        contentView.addConstraint(NSLayoutConstraint.init(item: arrowImage as Any, attribute: .leading, relatedBy: .equal, toItem: self.contentView, attribute: .leading, multiplier: 1.0, constant: 15))
+        contentView.addConstraint(NSLayoutConstraint.init(item: arrowImage, attribute: .top, relatedBy: .equal, toItem: self.contentView, attribute: .top, multiplier: 1.0, constant: 8))
+        contentView.addConstraint(NSLayoutConstraint.init(item: arrowImage, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: 25))
+        contentView.addConstraint(NSLayoutConstraint.init(item: arrowImage, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: 25))
+                
+        contentView.addConstraint(NSLayoutConstraint.init(item: logLabel, attribute: .leading, relatedBy: .equal, toItem: self.arrowImage, attribute: .trailing, multiplier: 1.0, constant: 5))
+        contentView.addConstraint(NSLayoutConstraint.init(item: logLabel, attribute: .top, relatedBy: .equal, toItem: self.arrowImage, attribute: .top, multiplier: 1.0, constant: 0))
+        contentView.addConstraint(NSLayoutConstraint.init(item: logLabel, attribute: .trailing, relatedBy: .equal, toItem: self.contentView, attribute: .trailing, multiplier: 1.0, constant: -15))
+        contentView.addConstraint(NSLayoutConstraint.init(item: logLabel, attribute: .bottom, relatedBy: .equal, toItem: self.contentView, attribute: .bottom, multiplier: 1.0, constant: -8))
         
-        logLabel = UILabel.init()
-        logLabel?.translatesAutoresizingMaskIntoConstraints = false
-        logLabel?.textColor = .black
-        logLabel?.font = .systemFont(ofSize: 12)
-        logLabel?.numberOfLines = 0;
-        self.contentView.addSubview(logLabel!)
-        self.contentView.addConstraint(NSLayoutConstraint.init(item: logLabel!, attribute: .leading, relatedBy: .equal, toItem: self.arrowImage, attribute: .trailing, multiplier: 1.0, constant: 5))
-        self.contentView.addConstraint(NSLayoutConstraint.init(item: logLabel!, attribute: .top, relatedBy: .equal, toItem: self.arrowImage, attribute: .top, multiplier: 1.0, constant: 0))
-        self.contentView.addConstraint(NSLayoutConstraint.init(item: logLabel!, attribute: .trailing, relatedBy: .equal, toItem: self.contentView, attribute: .trailing, multiplier: 1.0, constant: -15))
-        self.contentView.addConstraint(NSLayoutConstraint.init(item: logLabel!, attribute: .bottom, relatedBy: .equal, toItem: self.contentView, attribute: .bottom, multiplier: 1.0, constant: -8))
     }
     
     func renderWithModel(model:LogModel) {
         guard let content = model.content else {
             return
         }
-        self.logLabel?.text = "\(model.dateFormat)\n\(content)"
+        logLabel.text = "\(model.dateFormat)\n\(content)"
         let iconName = model.expand ? "doraemon_expand":"doraemon_expand_no"
-        arrowImage?.image = DKImage(named: iconName)
+        arrowImage.image = DKImage(named: iconName)
     }
 }
--- iOS/Swift/DoKitSwift/Src/Plugins/Common/Logs/Views/LogSwitchStyleCell.swift
+++ iOS/Swift/DoKitSwift/Src/Plugins/Common/Logs/Views/LogSwitchStyleCell.swift
@@ -9,14 +9,20 @@ import UIKit
 typealias switchBlock = (Bool) -> ()
 
 class LogSwitchStyleCell: UITableViewCell {
-
-    var titleLabel:UILabel?
-    var switchButton:UISwitch?
+    static let identifier: String = "LogSwitchStyleCellID"
     var switchOnBlock:switchBlock?
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
+    
+    lazy var titleLabel: UILabel = {
+        $0.font = .systemFont(ofSize: 17.0)
+        $0.textColor = .black
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        return $0
+    }(UILabel())
+    lazy var switchButton: UISwitch = {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.addTarget(self, action: #selector(self.switchStateChange(switchButton:)), for: .touchUpInside)
+        return $0
+    }(UISwitch())
 
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
@@ -29,31 +35,19 @@ class LogSwitchStyleCell: UITableViewCell {
     
     func initUI() {
         self.selectionStyle = .none
-        titleLabel = UILabel.init()
-        titleLabel?.font = .systemFont(ofSize: 17.0)
-        titleLabel?.textColor = .black
-        titleLabel?.translatesAutoresizingMaskIntoConstraints = false
-        contentView.addSubview(titleLabel!)
-        contentView.addConstraint(NSLayoutConstraint.init(item: titleLabel!, attribute: .leading, relatedBy: .equal, toItem: contentView, attribute: .leading, multiplier: 1.0, constant: 15))
-        contentView.addConstraint(NSLayoutConstraint.init(item: titleLabel!, attribute:.centerY, relatedBy: .equal, toItem: contentView, attribute: .centerY, multiplier: 1.0, constant: 0))
-        switchButton = UISwitch.init()
-        switchButton?.translatesAutoresizingMaskIntoConstraints = false
-        switchButton?.addTarget(self, action: #selector(self.switchStateChange(switchButton:)), for: .touchUpInside)
-        contentView.addSubview(switchButton!)
-        contentView.addConstraint(NSLayoutConstraint.init(item: switchButton!, attribute: .leading, relatedBy: .equal, toItem: titleLabel, attribute: .trailing, multiplier: 1.0, constant: 10))
-        contentView.addConstraint(NSLayoutConstraint.init(item: switchButton!, attribute: .trailing, relatedBy: .equal, toItem: contentView, attribute: .trailing, multiplier: 1.0, constant: -15))
-        contentView.addConstraint(NSLayoutConstraint.init(item: switchButton!, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 0.0, constant: 64))
-        contentView.addConstraint(NSLayoutConstraint.init(item: switchButton!, attribute:.centerY, relatedBy: .equal, toItem: contentView, attribute: .centerY, multiplier: 1.0, constant: 0))
+        contentView.addSubview(titleLabel)
+        contentView.addSubview(switchButton)
+
+        contentView.addConstraint(NSLayoutConstraint.init(item: titleLabel, attribute: .leading, relatedBy: .equal, toItem: contentView, attribute: .leading, multiplier: 1.0, constant: 15))
+        contentView.addConstraint(NSLayoutConstraint.init(item: titleLabel, attribute:.centerY, relatedBy: .equal, toItem: contentView, attribute: .centerY, multiplier: 1.0, constant: 0))
+        
+        contentView.addConstraint(NSLayoutConstraint.init(item: switchButton, attribute: .leading, relatedBy: .equal, toItem: titleLabel, attribute: .trailing, multiplier: 1.0, constant: 10))
+        contentView.addConstraint(NSLayoutConstraint.init(item: switchButton, attribute: .trailing, relatedBy: .equal, toItem: contentView, attribute: .trailing, multiplier: 1.0, constant: -15))
+        contentView.addConstraint(NSLayoutConstraint.init(item: switchButton, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 0.0, constant: 64))
+        contentView.addConstraint(NSLayoutConstraint.init(item: switchButton, attribute:.centerY, relatedBy: .equal, toItem: contentView, attribute: .centerY, multiplier: 1.0, constant: 0))
     }
     @objc func switchStateChange(switchButton:UISwitch) {
-        if (self.switchOnBlock != nil) {
-            self.switchOnBlock!(switchButton.isOn)
-        }
+        switchOnBlock?(switchButton.isOn)
     }
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
+    
 }
