3fef1cad34b5b3ce893d43ae445c60c51d7f8a1d
Desert <zyallday@gmail.com>
2020-06-27 16:58:00 +0800

    feat: 位置模拟
    Signed-off-by: Desert <zyallday@gmail.com>

--- Android/kotlin/doraemonkit/src/main/java/com/didichuxing/doraemonkit/kit/gpsmock/GpsMockKit.kt
+++ Android/kotlin/doraemonkit/src/main/java/com/didichuxing/doraemonkit/kit/gpsmock/GpsMockKit.kt
@@ -6,7 +6,7 @@ import com.didichuxing.doraemonkit.R
 import com.didichuxing.doraemonkit.aop.DokitPluginConfig
 import com.didichuxing.doraemonkit.constant.FragmentIndex
 import com.didichuxing.doraemonkit.kit.AbstractKit
-import com.didichuxing.doraemonkit.util.DokitUtil
+import com.didichuxing.doraemonkit.util.DokitUtil.getString
 
 /**
  * Created by wanglikun on 2018/9/20.
@@ -19,13 +19,21 @@ class GpsMockKit : AbstractKit() {
         get() = R.mipmap.dk_gps_mock
 
     override fun onClick(context: Context?) {
-        kotlinTip()
+        if (!DokitPluginConfig.SWITCH_DOKIT_PLUGIN) {
+            ToastUtils.showShort(getString(R.string.dk_plugin_close_tip))
+            return
+        }
+
+        if (!DokitPluginConfig.SWITCH_GPS) {
+            ToastUtils.showShort(getString(R.string.dk_plugin_gps_close_tip))
+            return
+        }
+
+        startUniversalActivity(FragmentIndex.FRAGMENT_GPS_MOCK)
     }
 
     override fun onAppInit(context: Context?) {}
-    override val isInnerKit: Boolean
-        get() = true
-
+    override val isInnerKit: Boolean = true
     override fun innerKitId(): String {
         return "dokit_sdk_comm_ck_gps"
     }
