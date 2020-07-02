c9c49a4b09295ea20a5ddfad43596fd5b816394e
jackjintai <jackjintai@didiglobal.com>
2020-07-02 18:40:58 +0800

    android:parse json and xml

--- Android/java/doraemonkit/src/main/java/com/didichuxing/doraemonkit/kit/filemanager/action/file/SaveFileAction.kt
+++ Android/java/doraemonkit/src/main/java/com/didichuxing/doraemonkit/kit/filemanager/action/file/SaveFileAction.kt
@@ -1,7 +1,14 @@
 package com.didichuxing.doraemonkit.kit.filemanager.action.file
 
+import android.util.Xml
 import com.blankj.utilcode.util.FileIOUtils
 import com.blankj.utilcode.util.FileUtils
+import com.blankj.utilcode.util.JsonUtils
+import org.json.JSONObject
+import org.xml.sax.ContentHandler
+import org.xml.sax.helpers.DefaultHandler
+import org.xmlpull.v1.XmlPullParser
+import java.lang.Exception
 
 /**
  * ================================================
@@ -15,10 +22,29 @@ import com.blankj.utilcode.util.FileUtils
 object SaveFileAction {
     fun saveFileRes(content: String, filePath: String): MutableMap<String, Any> {
         val response = mutableMapOf<String, Any>()
+
         if (FileUtils.isFileExists(filePath)) {
-            FileIOUtils.writeFileFromString(filePath, content, false)
-            response["code"] = 200
-            response["success"] = true
+            val fileExtension = FileUtils.getFileExtension(filePath)
+            if (fileExtension.contains("xml")) {
+                try {
+                    Xml.parse(content, DefaultHandler())
+                } catch (e: Exception) {
+                    response["code"] = 0
+                    response["success"] = false
+                }
+
+            } else if (fileExtension.contains("json")) {
+                try {
+                    JSONObject(content)
+                } catch (e: Exception) {
+                    response["code"] = 0
+                    response["success"] = false
+                }
+            } else {
+                FileIOUtils.writeFileFromString(filePath, content, false)
+                response["code"] = 200
+                response["success"] = true
+            }
         } else {
             response["code"] = 0
             response["success"] = false
