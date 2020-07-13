380f02601bd42efd977e5b7099ad8be7685baa9b
kinarobin <kinarobin@outlook.com>
2020-07-13 12:28:30 +0800

    optimize fetch image data

--- iOS/DoraemonKit/Src/Core/Plugin/Performance/LargeImageDetection/Function/UIImageView+DoraemonSDImage.m
+++ iOS/DoraemonKit/Src/Core/Plugin/Performance/LargeImageDetection/Function/UIImageView+DoraemonSDImage.m
@@ -14,55 +14,50 @@
 
 @implementation UIImageView (DoraemonSDImage)
 
-+ (void)load{
++ (void)load {
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wundeclared-selector"
     
-    Method originAddObserverMethod = class_getInstanceMethod(self, @selector(sd_setImageWithURL:placeholderImage:options:context:progress:completed:));
-    if (originAddObserverMethod) {
+    BOOL respondToSelector = [UIImageView instancesRespondToSelector:@selector(sd_internalSetImageWithURL:placeholderImage:options:context:setImageBlock:progress:completed:)];
+    if (respondToSelector) {
         // 兼容SDWebImage 最新版本 5.0.6
-        [self doraemon_swizzleInstanceMethodWithOriginSel:@selector(sd_setImageWithURL:placeholderImage:options:context:progress:completed:)
-                                              swizzledSel:@selector(doraemon_sd_setImageWithURL:placeholderImage:options:context:progress:completed:)];
-    }else{
+        [self doraemon_swizzleInstanceMethodWithOriginSel:@selector(sd_internalSetImageWithURL:placeholderImage:options:context:setImageBlock:progress:completed:)
+                                              swizzledSel:@selector(doraemon_sd_internalSetImageWithURL:placeholderImage:options:context:setImageBlock:progress:completed:)];
+    } else {
         // 兼容SDWebImage 我们使用的版本 3.7.6
         [self doraemon_swizzleInstanceMethodWithOriginSel:@selector(sd_setImageWithURL:placeholderImage:options:progress:completed:) swizzledSel:@selector(doraemon_sd_setImageWithURL:placeholderImage:options:progress:completed:)];
     }
-    //估计以后还会兼容其他版本啊。哭。
-    #pragma clang diagnostic pop
+#pragma clang diagnostic pop
 }
 
-- (void)doraemon_sd_setImageWithURL:(nullable NSURL *)url
-          placeholderImage:(nullable UIImage *)placeholder
-                   options:(NSUInteger)options
-                   context:(id)context
-                  progress:(id)progressBlock
-                          completed:(void(^)(UIImage * _Nullable image, NSError * _Nullable error, NSInteger cacheType, NSURL * _Nullable imageURL))completedBlock{
+- (void)doraemon_sd_internalSetImageWithURL:(nullable NSURL *)url
+                           placeholderImage:(nullable UIImage *)placeholder
+                                    options:(NSUInteger)options
+                                    context:(id)context
+                              setImageBlock:(id)setImageBlock
+                                   progress:(id)progressBlock
+                                  completed:(void(^)(UIImage * _Nullable image, NSData * _Nullable data, NSError * _Nullable error, NSInteger cacheType, BOOL finished, NSURL * _Nullable imageURL))completedBlock {
     
     __weak typeof(self) weafSelf = self;
     if ([DoraemonLargeImageDetectionManager shareInstance].detecting) {
-        id replaceCompletedBlock = ^(UIImage * _Nullable image, NSError * _Nullable error, NSInteger cacheType, NSURL * _Nullable imageURL){
-            NSData *data = UIImageJPEGRepresentation(image, 1.0);
-            if (!data && data.length <= 0) {
-                data = UIImagePNGRepresentation(image);
-            }
+        id replaceCompletedBlock = ^(UIImage * _Nullable image, NSData * _Nullable data, NSError * _Nullable error, NSInteger cacheType,BOOL finished, NSURL * _Nullable imageURL) {
             if (data.length > [DoraemonLargeImageDetectionManager shareInstance].minimumDetectionSize) {
                 NSString *drawText = [NSString stringWithFormat:@"url : %@ \n size : %fKB",[url absoluteString],data.length/1024.];
                 weafSelf.image = [self drawText:drawText inImage:weafSelf.image];
             }
             if (completedBlock) {
-                completedBlock(weafSelf.image, error, cacheType,imageURL);
+                completedBlock(weafSelf.image, data, error, cacheType, finished, imageURL);
             }
-            
         };
-        [self doraemon_sd_setImageWithURL:url placeholderImage:placeholder options:options context:context progress:progressBlock completed:replaceCompletedBlock];
-    }else{
-        [self doraemon_sd_setImageWithURL:url placeholderImage:placeholder options:options context:context progress:progressBlock completed:completedBlock];
+        [self doraemon_sd_internalSetImageWithURL:url placeholderImage:placeholder options:options context:context setImageBlock:setImageBlock progress:progressBlock completed:replaceCompletedBlock];
+    } else {
+        [self doraemon_sd_internalSetImageWithURL:url placeholderImage:placeholder options:options context:context setImageBlock:setImageBlock progress:progressBlock completed:completedBlock];
     }
 }
 
 - (void)doraemon_sd_setImageWithURL:(NSURL *)url placeholderImage:(UIImage *)placeholder options:(NSUInteger)options progress:(id)progressBlock completed:(void(^)(UIImage *image, NSError *error, NSInteger cacheType, NSURL *imageURL))completedBlock{
     __weak typeof(self) weafSelf = self;
-    if ([DoraemonLargeImageDetectionManager shareInstance].detecting){
+    if ([DoraemonLargeImageDetectionManager shareInstance].detecting) {
         id replaceCompletedBlock = ^(UIImage *image, NSError *error, NSInteger cacheType, NSURL *imageURL) {
             NSData *data = UIImageJPEGRepresentation(image, 1.0);
             if (!data && data.length <= 0) {
@@ -77,16 +72,16 @@
             }
         };
         [self doraemon_sd_setImageWithURL:url placeholderImage:placeholder options:options progress:progressBlock completed:replaceCompletedBlock];
-    }else{
+    } else {
         [self doraemon_sd_setImageWithURL:url placeholderImage:placeholder options:options progress:progressBlock completed:completedBlock];
     }
 }
 
-- (UIImage *)drawText:(NSString *)text inImage:(UIImage *)image{
+- (UIImage *)drawText:(NSString *)text inImage:(UIImage *)image {
     UIFont *font = [UIFont boldSystemFontOfSize:12 * (image.size.height / 230)];
     UIGraphicsBeginImageContextWithOptions(image.size, NO, [UIScreen mainScreen].scale);
-    [image drawInRect:CGRectMake(0, 0, image.size.width, image.size.height)];
     CGRect rect = CGRectMake(0, 0, image.size.width, image.size.height);
+    [image drawInRect:rect];
     [text drawInRect:CGRectIntegral(rect) withAttributes:@{
                                                            NSFontAttributeName : font,
                                                            NSForegroundColorAttributeName : [UIColor redColor]
