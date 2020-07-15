de80398b396350a28595db74c7c441f3ca7698e6
jackjintai <jackjintai@didiglobal.com>
2020-07-15 19:47:24 +0800

    android:新增文件同步助手

--- Android/java/doraemonkit-no-op/upload.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-echo -n  "please enter bintray userid ->"
-read  userid_bintray
-echo -n  "please enter bintray apikey ->"
-read  apikey_bintray
-../gradlew clean build bintrayUpload -PbintrayUser=${userid_bintray} -PbintrayKey=${apikey_bintray} -PdryRun=false
