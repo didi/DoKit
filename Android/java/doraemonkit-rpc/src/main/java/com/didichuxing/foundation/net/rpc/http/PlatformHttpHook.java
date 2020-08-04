aa0083c4015045d9446d212c11cff702283eeff6
jackjintai <jackjintai@didiglobal.com>
2020-08-04 11:26:01 +0800

    android:新增缓存清理自定义选项

--- Android/java/doraemonkit-rpc/src/main/java/com/didichuxing/foundation/net/rpc/http/PlatformHttpHook.java
+++ Android/java/doraemonkit-rpc/src/main/java/com/didichuxing/foundation/net/rpc/http/PlatformHttpHook.java
@@ -81,7 +81,7 @@ public class PlatformHttpHook {
      */
     private static List<Interceptor> removeDuplicate(List<Interceptor> list) {
         //保证顺序并去重
-        LinkedHashSet h = new LinkedHashSet<Interceptor>(list);
+        LinkedHashSet<Interceptor> h = new LinkedHashSet<>(list);
         list.clear();
         list.addAll(h);
         return list;
