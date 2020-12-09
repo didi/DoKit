e4b63f2f618314e19b366a9191b41b62360d6240
albert <jkdev123cool@gmail.com>
2020-12-09 21:28:10 +0800

    iOS: 编辑NSUserDefaults页面展示当前编辑的key & 调低编辑高度

--- iOS/DoraemonKit/Src/Core/Plugin/Common/NSUserDefaults/ViewControllers/DoraemonNSUserDefaultsEditViewController.m
+++ iOS/DoraemonKit/Src/Core/Plugin/Common/NSUserDefaults/ViewControllers/DoraemonNSUserDefaultsEditViewController.m
@@ -10,7 +10,8 @@
 #import "DoraemonNSUserDefaultsModel.h"
 
 @interface DoraemonNSUserDefaultsEditViewController ()
-@property (nonatomic, strong) UITextView *textView;
+@property (nonatomic, strong) UITextView *keyTextView;
+@property (nonatomic, strong) UITextView *valueTextView;
 @end
 
 @implementation DoraemonNSUserDefaultsEditViewController
@@ -26,10 +27,23 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.textView = [[UITextView alloc] initWithFrame:CGRectMake(0, IPHONE_NAVIGATIONBAR_HEIGHT, self.view.doraemon_width, self.view.doraemon_height-IPHONE_NAVIGATIONBAR_HEIGHT)];
-    [self.view addSubview:self.textView];
+    
+    self.keyTextView = [[UITextView alloc] init];
+    self.keyTextView.editable = NO;
+    self.keyTextView.font = [UIFont systemFontOfSize:16];
+    [self.view addSubview:self.keyTextView];
+    self.keyTextView.text = self.model.key;
+    CGFloat keyMaxWidth = self.view.doraemon_width;
+    CGFloat keyMaxHeight = 60;
+    CGSize keySize = [self.keyTextView sizeThatFits:CGSizeMake(keyMaxWidth, keyMaxHeight)];
+    self.keyTextView.frame = CGRectMake(0, IPHONE_NAVIGATIONBAR_HEIGHT + 10, keyMaxWidth, keySize.height + 1);
+    
+    self.valueTextView = [[UITextView alloc] initWithFrame:CGRectMake(0, CGRectGetMaxY(self.keyTextView.frame) + 10, self.view.doraemon_width, 300)];
+    [self.view addSubview:self.valueTextView];
+    self.valueTextView.layer.borderWidth = 0.5;
+    self.valueTextView.layer.borderColor = [[UIColor lightGrayColor] CGColor];
     self.title = self.model.key;
-    self.textView.text = [self.model.value description];
+    self.valueTextView.text = [self.model.value description];
     
     UIBarButtonItem *item = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemDone target:self action:@selector(submit)];
     self.navigationItem.rightBarButtonItems = @[item];
@@ -37,7 +51,7 @@
 }
 
 - (void)submit {
-    [[NSUserDefaults standardUserDefaults] setObject:self.textView.text forKey:_model.key];
+    [[NSUserDefaults standardUserDefaults] setObject:self.valueTextView.text forKey:_model.key];
     [self.navigationController popViewControllerAnimated:true];
 }
 
