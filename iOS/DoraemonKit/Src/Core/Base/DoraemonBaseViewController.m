6a35de0c4067a8aa9f662df2b3f37b1041d10a7c
albert <jkdev123cool@gmail.com>
2021-05-06 16:32:51 +0800

    fix: 深色模式导航条返回按钮未自动切换的问题

--- iOS/DoraemonKit/Src/Core/Base/DoraemonBaseViewController.m
+++ iOS/DoraemonKit/Src/Core/Base/DoraemonBaseViewController.m
@@ -17,6 +17,7 @@
  
 @property (nonatomic, strong) DoraemonNavBarItemModel *leftModel;
 
+@property (nonatomic, strong) NSArray *leftNavBarItemArray;
 @end
 
 @implementation DoraemonBaseViewController
@@ -85,6 +86,7 @@
             } else {
                 self.leftModel.image = [UIImage doraemon_xcassetImageNamed:@"doraemon_back"];
             }
+            [self setLeftNavBarItems:self.leftNavBarItemArray];
         }
     }
 #endif
@@ -113,6 +115,7 @@
 }
 
 - (void)setLeftNavBarItems:(NSArray *)items{
+    _leftNavBarItemArray = items;
     NSArray *barItems = [self navigationItems:items];
     if (barItems) {
         self.navigationItem.leftBarButtonItems = barItems;
