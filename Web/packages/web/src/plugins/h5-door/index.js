9c5d5937accfea3cbbf2ef3e9e2b41cdde8b4d91
YusenZhang826 <1239216641@qq.com>
2021-05-30 21:11:18 +0800

    h5door final version

--- Web/packages/web/src/plugins/h5-door/index.js
+++ Web/packages/web/src/plugins/h5-door/index.js
@@ -1,10 +1,11 @@
 import HelloWorld from './ToolHelloWorld.vue'
 import {RouterPlugin} from '@dokit/web-core'
+import H5Door from './ToolH5Door.vue'
 
 export default new RouterPlugin({
   nameZh: '任意门',
   name: 'h5-door',
   icon: 'https://pt-starimg.didistatic.com/static/starimg/img/FHqpI3InaS1618997548865.png',
-  component: HelloWorld
+  component: H5Door
 })
 
