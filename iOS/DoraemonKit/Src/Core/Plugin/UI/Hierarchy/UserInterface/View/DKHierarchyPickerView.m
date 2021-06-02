acddc1fe32176bc55cdaa496ff7316a288a89efa
ChasonTang <chasontang@warmbloom.com>
2021-06-02 20:32:13 +0800

    feat(iOS): Refactor UI Hierarchy (#839)
    * feat(iOS): Add .gitignore
    
    1. Fix Podfile
    2. Refactor DKMoveView DKPickerView
    
    * chore(iOS): Add missing file

--- iOS/DoraemonKit/Src/Core/Plugin/UI/Hierarchy/UserInterface/View/DKHierarchyPickerView.m
+++ iOS/DoraemonKit/Src/Core/Plugin/UI/Hierarchy/UserInterface/View/DKHierarchyPickerView.m
@@ -1,25 +1,28 @@
-//
-//  DoraemonHierarchyPickerView.m
-//  DoraemonKit-DoraemonKit
-//
-//  Created by lijiahuan on 2019/11/2.
-//
+#import <DoraemonKit/DKHierarchyPickerView.h>
+#import <DoraemonKit/DoraemonHierarchyHelper.h>
 
-#import "DoraemonHierarchyPickerView.h"
-#import "DoraemonHierarchyHelper.h"
-#import "UIView+Doraemon.h"
+NS_ASSUME_NONNULL_BEGIN
 
-@implementation DoraemonHierarchyPickerView
+@interface DKHierarchyPickerView ()
+
+- (nullable NSArray<UIView *> *)viewForSelectionAtPoint:(CGPoint)tapPointInWindow;
+
+@end
+
+NS_ASSUME_NONNULL_END
+
+@implementation DKHierarchyPickerView
 
 #pragma mark - Over write
+
 - (void)viewDidUpdateOffset:(UIPanGestureRecognizer *)sender offset:(CGPoint)offsetPoint {
-    NSArray <UIView *>*views = [self viewForSelectionAtPoint:self.center];
-    [self.delegate doraemonHierarchyView:self didMoveTo:views];
+    NSArray <UIView *> *views = [self viewForSelectionAtPoint:self.center];
+    [self.delegate hierarchyView:self didMoveTo:views];
 }
 
 #pragma mark - Primary
-- (NSArray<UIView *> *)viewForSelectionAtPoint:(CGPoint)tapPointInWindow
-{
+
+- (nullable NSArray<UIView *> *)viewForSelectionAtPoint:(CGPoint)tapPointInWindow {
     // Select in the window that would handle the touch, but don't just use the result of hitTest:withEvent: so we can still select views with interaction disabled.
     // Default to the the application's key window if none of the windows want the touch.
 #pragma clang diagnostic push
@@ -32,25 +35,24 @@
             break;
         }
     }
-    
+
     // Select the deepest visible view at the tap point. This generally corresponds to what the user wants to select.
     return [self recursiveSubviewsAtPoint:tapPointInWindow inView:windowForSelection skipHiddenViews:YES];
 }
 
-- (NSArray<UIView *> *)recursiveSubviewsAtPoint:(CGPoint)pointInView inView:(UIView *)view skipHiddenViews:(BOOL)skipHidden
-{
+- (NSArray<UIView *> *)recursiveSubviewsAtPoint:(CGPoint)pointInView inView:(UIView *)view skipHiddenViews:(BOOL)skipHidden {
     NSMutableArray<UIView *> *subviewsAtPoint = [NSMutableArray array];
     for (UIView *subview in view.subviews) {
         BOOL isHidden = subview.hidden || subview.alpha < 0.01;
         if (skipHidden && isHidden) {
             continue;
         }
-        
+
         BOOL subviewContainsPoint = CGRectContainsPoint(subview.frame, pointInView);
         if (subviewContainsPoint) {
             [subviewsAtPoint addObject:subview];
         }
-        
+
         // If this view doesn't clip to its bounds, we need to check its subviews even if it doesn't contain the selection point.
         // They may be visible and contain the selection point.
         if (subviewContainsPoint || !subview.clipsToBounds) {
