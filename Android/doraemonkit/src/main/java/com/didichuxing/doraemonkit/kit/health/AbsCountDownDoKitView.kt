24ce369ae46d50bbdabb509caca35b293ba948ce
jtsky <704167880@qq.com>
2021-07-27 10:50:42 +0800

    modify: [Android]-新增sdk打包上传配置文件检测

--- Android/doraemonkit/src/main/java/com/didichuxing/doraemonkit/kit/health/AbsCountDownDoKitView.kt
+++ Android/doraemonkit/src/main/java/com/didichuxing/doraemonkit/kit/health/AbsCountDownDoKitView.kt
@@ -1,19 +1,6 @@
 package com.didichuxing.doraemonkit.kit.health
 
-import android.content.Context
-import android.view.Gravity
-import android.view.LayoutInflater
-import android.view.View
-import android.widget.FrameLayout
-import android.widget.TextView
-import com.didichuxing.doraemonkit.R
 import com.didichuxing.doraemonkit.kit.core.AbsDokitView
-import com.didichuxing.doraemonkit.kit.core.DokitViewLayoutParams
-import com.didichuxing.doraemonkit.kit.core.DokitViewManager
-import com.didichuxing.doraemonkit.util.ConvertUtils
-import com.didichuxing.doraemonkit.util.LogHelper
-import kotlinx.coroutines.*
-import kotlinx.coroutines.flow.*
 
 /**
  * ================================================
