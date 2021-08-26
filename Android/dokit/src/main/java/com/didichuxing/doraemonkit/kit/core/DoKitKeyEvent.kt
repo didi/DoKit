5b1df7c36ca534e42fce29c07fcf59b522654a9c
jtsky <704167880@qq.com>
2021-08-26 14:39:39 +0800

    modify: [Android]-add DoKitMCHummerHelper.java

---
+++ Android/dokit/src/main/java/com/didichuxing/doraemonkit/kit/core/DoKitKeyEvent.kt
@@ -0,0 +1,15 @@
+package com.didichuxing.doraemonkit.kit.core
+
+/**
+ * ================================================
+ * 作    者：jint（金台）
+ * 版    本：1.0
+ * 创建日期：2021/8/25-18:47
+ * 描    述：
+ * 修订历史：
+ * ================================================
+ */
+data class DoKitKeyEvent(
+    val action: Int,
+    val keyCode: Int
+)
