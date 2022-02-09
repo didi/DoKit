d39107af63b3a393197c300797d36ab2f80d9084
ChasonTang <chasontang@warmbloom.com>
2022-02-09 15:23:05 +0800

    feat(iOS): Complete event synthesize (#953)
    * chore: Temp commit
    
    * feat(iOS): Complete tap simulation
    
    1. Extract common.h
    2. Fix AppDelegate access control error
    
    * chore(iOS): Add missing file
    
    * fix(iOS): iOS 14 and below version support
    
    * fix(iOS): Add missing NDEBUG macro
    
    1. Complete tap and long press feature
    
    * feat(iOS): Complete pan gesture recognizer
    
    * feat(iOS): Add -[UIView dk_dragWithStartPoint:endPoint:stepCount:]
    
    * chore(iOS): Temp commit
    
    * feat(iOS): Complete pinch in/out gesture recognizer logic
    
    * feat(iOS): Complete swipe gesture recognizer logic
    
    * feat(iOS): Complete rotation gesture recognizer
    
    * fix(iOS): Use angle bracket to import

--- iOS/Demo/ViewController.swift
+++ iOS/Demo/ViewController.swift
@@ -6,12 +6,57 @@
 //
 
 import UIKit
+import DoraemonKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        var keyWindow: UIWindow? = nil
+        if #available(iOS 13.0, *) {
+            UIApplication.shared.connectedScenes.filter { scene in
+                return scene.activationState == .foregroundActive
+            }.forEach { scene in
+                if let windowScene = scene as? UIWindowScene {
+                    if #available(iOS 15.0, *) {
+                        assert(keyWindow == nil, "keyWindow has been assigned")
+                        keyWindow = windowScene.keyWindow
+                    } else {
+                        // Fallback on earlier versions
+                        assert(false)
+                    }
+                }
+            }
+        } else {
+            // Fallback on earlier versions
+            keyWindow = UIApplication.shared.keyWindow
+        }
+        
+        guard let keyWindow = keyWindow else {
+            assert(false, "keyWindow not found")
+            
+            return
+        }
+        
+        let touch = UITouch(point: CGPoint(x: 50, y: 50), window: keyWindow)
+        let event = eventWithTouches([touch])
+        guard let event = event else {
+            assert(false, "eventWithTouches() return nil")
+            
+            return
+        }
+        UIApplication.shared.sendEvent(event)
+        touch.dk_updateTimestamp(with: .ended)
+        UIApplication.shared.sendEvent(event)
+    }
+    
+    @IBAction private func buttonHandler(_ sender: Any) {
+        print("buttonHandler")
+    }
+    
 }
