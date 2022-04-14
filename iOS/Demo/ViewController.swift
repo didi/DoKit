ba2a53a91776444232444e2b1c28af2c5cb40cec
Chason Tang <tangjiacheng@acornsemi.com>
2022-04-14 13:34:05 +0800

    chore(iOS): Update Demo application license text.

--- iOS/Demo/ViewController.swift
+++ iOS/Demo/ViewController.swift
@@ -1,9 +1,18 @@
-//
-//  ViewController.swift
-//  Demo
-//
-//  Created by 唐佳诚 on 2021/9/22.
-//
+/**
+ * Copyright 2017 Beijing DiDi Infinity Technology and Development Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 import UIKit
 import DoraemonKit
@@ -13,50 +22,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        var keyWindow: UIWindow? = nil
-        if #available(iOS 13.0, *) {
-            UIApplication.shared.connectedScenes.filter { scene in
-                return scene.activationState == .foregroundActive
-            }.forEach { scene in
-                if let windowScene = scene as? UIWindowScene {
-                    if #available(iOS 15.0, *) {
-                        assert(keyWindow == nil, "keyWindow has been assigned")
-                        keyWindow = windowScene.keyWindow
-                    } else {
-                        // Fallback on earlier versions
-                        assert(false)
-                    }
-                }
-            }
-        } else {
-            // Fallback on earlier versions
-            keyWindow = UIApplication.shared.keyWindow
-        }
-        
-        guard let keyWindow = keyWindow else {
-            assert(false, "keyWindow not found")
-            
-            return
-        }
-        
-        let touch = UITouch(point: CGPoint(x: 50, y: 50), window: keyWindow)
-        let event = eventWithTouches([touch])
-        guard let event = event else {
-            assert(false, "eventWithTouches() return nil")
-            
-            return
-        }
-        UIApplication.shared.sendEvent(event)
-        touch.dk_updateTimestamp(with: .ended)
-        UIApplication.shared.sendEvent(event)
-    }
-    
-    @IBAction private func buttonHandler(_ sender: Any) {
-        print("buttonHandler")
     }
     
 }
