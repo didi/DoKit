7a1f778ede6d26e6a314043a38bf100d244a436f
lizihan <compact_set@foxmail.com>
2022-06-21 16:38:25 +0800

    新增快捷工具插件，包括刷新和生成二维码功能

--- Web/packages/web/src/feature.js
+++ Web/packages/web/src/feature.js
@@ -13,12 +13,13 @@ import HelloWorld from './components/ToolHelloWorld'
 import Resource from './plugins/resources/index'
 import ApiMock from './plugins/api-mock/index'
 import WebVitals from './plugins/web-vitals-time/index'
+import BasicTools from './plugins/basic-tools/index'
 
 import {IndependPlugin, RouterPlugin} from '@dokit/web-core'
 
 export const BasicFeatures = {
   title: '常用工具',
-  list: [Console, AppInfo, Resource, Network, Storage, DemoPlugin, DemoIndependPlugin, H5DoorPlugin, WebVitals, Element, OneMachineWithMultipleControls, scanCode]
+  list: [Console, AppInfo, Resource, Network, Storage, DemoPlugin, DemoIndependPlugin, H5DoorPlugin, WebVitals, Element, OneMachineWithMultipleControls, scanCode,BasicTools]
   // list: [Console, AppInfo, Resource, Network, Storage, H5DoorPlugin]
 }
 
---
+++ Web/packages/web/src/plugins/basic-tools/index.js
@@ -0,0 +1,9 @@
+import BaiscTools from './app.vue'
+import {RouterPlugin} from '@dokit/web-core'
+
+export default new RouterPlugin({
+    nameZh: '快捷工具',
+    name: 'basic-tools',
+    icon: 'https://pt-starimg.didistatic.com/static/starimg/img/jvD7qcMXX51645432343946.png',
+    component:BaiscTools
+})
